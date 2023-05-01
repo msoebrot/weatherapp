debdc67767ddbacd3371b42f0320331feaf1e957
Michael Soebroto <msoebrot@ucsc.edu>
2023-05-01 14:33:21 -0700

    Added javascript to HTML.
    Added JavaScript that allows the date to be changed, collect the geographical location of the user, and print out a string when pressing the button.

---
+++ script.js
@@ -0,0 +1,42 @@
+// Create the variable for latitude
+// Create the variable for longitude
+let latitude, longitude;
+
+window.onload = function() {
+    //gets today's date using the builtin Date object.
+    const date = new Date();
+
+    //extract the date from date object and format into string.
+    const dateString = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+
+    //set .date to date String
+    document.getElementById('date').innerHTML = dateString;
+
+    //check for location through navigator
+    if ("geolocation" in navigator) {
+        //if success, run success()
+		navigator.geolocation.getCurrentPosition(success)
+
+	} else {
+        //Otherwise print error message
+	    console.log("Geolocation is not available in your browser.");
+	}
+
+    // Set const btn to #getWeatherBtn ID
+    const btn = document.getElementById('getWeatherBtn');
+
+    btn.addEventListener('click', () => {
+        let forecast = [["M", 52],["Tu", 53],["W", 54],["Th", 55],["F", 56]];
+        let forecastElements = document.getElementsByClassName("forecast");
+        for (let i = 0; i < forecast.length; i++) {
+            forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u2109";
+        }
+    });
+}
+
+function success(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+    // Print out the latittude and longitude to see if it works!
+}
