777bbc5d6e89aa50b49428be50bc7394a7123cfd
Michael Soebroto <msoebrot@ucsc.edu>
2023-05-01 17:57:29 -0700

    Update script.js
    Change unicode of degree Fahrenheit to match demo.

--- script.js
+++ script.js
@@ -37,6 +37,6 @@ btn.addEventListener('click', function() {
     let forecast = [["M", 52],["Tu", 53],["W", 54],["Th", 55],["F", 56]];
     let forecastElements = document.getElementsByClassName("forecast");
     for (let i = 0; i < forecast.length; i++) {
-        forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u2109";
+        forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u00B0F";
     }
 });
